f23ec1887cf3ace5092bb5963bb3dc499c53d66d
Tito-74 <kipkirui133@gmail.com>
2021-09-26 01:48:05 +0300

    adding manage.py file

---
+++ manage.py
@@ -0,0 +1,38 @@
+from app import create_app,db
+from  flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager,Server
+from app.models import Like, Quotes,Comment,Post,User
+
+
+# creating app instance
+app = create_app('development')
+# app = create_app('production')
+
+
+migrate = Migrate(app,db)
+manager = Manager(app)
+
+manager.add_command('server',Server)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+
+
+
+def test():
+
+    """
+    Run the unit tests.
+
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,Quotes=Quotes,Comment=Comment, Post=Post, User=User,Like=Like) 
+
+if __name__ == '__main__':
+    manager.run()
