4edade9ed21defceb0ea7a0c857b7782eb873516
Tito-74 <kipkirui133@gmail.com>
2021-09-26 01:39:11 +0300

    adding styling and assets folder

---
+++ app/static/js/script.js
@@ -0,0 +1,112 @@
+
+    // {/* // let like = document.querySelector('#like');
+    // // let dislike =document.querySelector('#dislike');
+    // // let input1 = document.querySelector('#input1');
+    // // let input2 = document.querySelector('#input2');
+    
+  
+    // // like.addEventListener('click',()=>{
+    // //   input1.value = parseInt(input1.value) + 1;
+    // //   input1.style.color = "#12ff00";
+  
+    // // })
+  
+    // // dislike.addEventListener('click',()=>{
+    // //   input2.value = parseInt(input2.value) + 1;
+    // //   input2.style.color = "#12ff00";
+  
+    // }) 
+    var count=0;
+   
+    function upvote(){
+        count++;
+        document.getElementById('like').innerHTML=count;
+    }
+    var dislike=0;
+    function downvote(){
+        dislike++;
+        document.getElementById('dislike').innerHTML=dislike;
+    }
+    // second thumb
+    var dislike1=0
+    function downvote1(){
+        dislike1++;
+        document.getElementById('dislike1').innerHTML=dislike1;
+    }
+    var count1=0
+    function upvote1(){
+        count1++;
+        document.getElementById('like1').innerHTML=count1;
+    }
+
+    // icon3
+  var thumb=0;
+  function thumbs(){
+      thumb++;
+      document.getElementById('likes2').innerHTML=thumb;
+  }
+  var thumbed=0;
+  function mythumb(){
+      thumbed++;
+      document.getElementById('down').innerHTML=thumbed;
+  }
+//   thumb4
+var count3=0;
+   
+function upvote3(){
+    count3++;
+    document.getElementById('like3').innerHTML=count3;
+}
+var dislike3=0;
+function downvote3(){
+    dislike3++;
+    document.getElementById('dislike3').innerHTML=dislike3;
+}
+// fiveth thumb
+var count4=0;
+   
+function upvote4(){
+    count4++;
+    document.getElementById('like4').innerHTML=count4;
+}
+var dislike4=0;
+function downvote4(){
+    dislike4++;
+    document.getElementById('dislike4').innerHTML=dislike4;
+}
+// six thumb
+var count5=0;
+   
+function upvote5(){
+    count5++;
+    document.getElementById('like5').innerHTML=count5;
+}
+var dislike5=0;
+function downvote5(){
+    dislike5++;
+    document.getElementById('dislike5').innerHTML=dislike5;
+}
+// seventh thumb
+var count6=0;
+   
+function upvote6(){
+    count6++;
+    document.getElementById('like6').innerHTML=count6;
+}
+var dislike6=0;
+function downvote6(){
+    dislike6++;
+    document.getElementById('dislike6').innerHTML=dislike6;
+}
+// eith thumb
+var count7=0;
+   
+function upvote7(){
+    count7++;
+    document.getElementById('like7').innerHTML=count7;
+}
+var dislike7=0;
+function downvote7(){
+    dislike7++;
+    document.getElementById('dislike7').innerHTML=dislike7;
+}
