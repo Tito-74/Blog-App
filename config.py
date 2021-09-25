7f1f48475d4bbc485c0f52f850484e2d26158a4e
Tito-74 <kipkirui133@gmail.com>
2021-09-26 02:08:09 +0300

    modifying index

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(32)
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:2574@localhost/myblog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:2574@localhost/blogapp'
     
     
    
