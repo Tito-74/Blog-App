7ca6e543dcd7e3333d32db2f94f48c47445ef278
Tito-74 <kipkirui133@gmail.com>
2021-09-26 01:47:17 +0300

    adding config file

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+
+
+class Config:
+    '''
+    General configuration parent class
+
+    '''
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.urandom(32)
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:2574@localhost/myblog'
+    
+    
+   
+
+
+   
+    
+class ProdConfig(Config):
+    '''
+    production configuration child class
+
+    Args:
+        config:  The parent configuration class with General configuration settings
+
+    '''
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    '''
+    DEBUG = True
+
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}    
