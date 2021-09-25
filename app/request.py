3951f745eb691580a17f549980d4c95cd31cb9dc
Tito-74 <kipkirui133@gmail.com>
2021-09-26 01:44:15 +0300

    adding request.py file

---
+++ app/request.py
@@ -0,0 +1,13 @@
+import urllib.request,json
+from .models import Quotes
+
+
+def get_quote():
+    '''
+    Function that gets the random quotes
+    '''
+    get_article_details_url = 'http://quotes.stormconsultancy.co.uk/random.json'.format()
+    with urllib.request.urlopen(get_article_details_url) as url:
+        quote_data = url.read()
+        quote_response = json.loads(quote_data)
+    return quote_response
